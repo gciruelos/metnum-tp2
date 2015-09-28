2b2001d3c4257f950df96f88b8fe5c6bee05a528
gciruelos <gonzalo.ciruelos@gmail.com>
2015-09-28 02:29:09 -0300

    terminado el codigo! pasamos todos los tests

--- src/matriz_deportes.h
+++ src/matriz_deportes.h
@@ -10,14 +10,11 @@
 
 typedef unsigned int uint;
 
-uint first_node = 1;
-
-
 class MatrizDep {
 
 public:
 
-    MatrizDep(std::ifstream& in){ 
+    MatrizDep(std::ifstream& in, double c){ 
         uint fecha, equipo1, goles1, equipo2, goles2; 
         uint nodes, edges;
 
@@ -25,19 +22,88 @@ public:
         
         in >> nodes >> edges; // leo nodos y edges 
         
+        std::vector<std::vector<double> >  h;
         // armo la matriz
-        for(uint i = 0; i<nodes; i++)
-            m.push_back(std::vector<uint>(nodes, 0));
+        for(uint i = 0; i<nodes; i++){
+            h.push_back(std::vector<double>(nodes, 0));
+            m.push_back(std::vector<double>(nodes, 0));
+        }
 
         for(uint i = 0; i < edges; i++){
-             in >> fecha >> equipo1 >> goles1 >> equpo2 >> goles2;
-             // los nodos se numeran desde first_node...
-             equipo1-=first_node; equipo2-=first_node;    
+             in >> fecha >> equipo1 >> goles1 >> equipo2 >> goles2;
+             // los nodos se numeran desde 1...
+             equipo1--; equipo2--;
+             if(goles1 > goles2){
+                 h[equipo2][equipo1] += goles1-goles2;
+             } else if (goles1 < goles2){
+                 h[equipo1][equipo2] += goles2-goles1;
+             } else {
+                 // ver que hacemos con el empate, por ahora nada
+             }
         }
+
+        // normalizo segun lo que dice GeM 
+        std::vector<double> goles;
+        for(int i = 0; i<nodes; i++){
+            double accum = 0;
+            for(int j = 0; j<nodes; j++){
+                accum+=h[i][j];
+            }
+            goles.push_back(accum);
+        }
+        mostrar_vector(goles, std::cerr);
+        std::cerr << std::endl;
+
+        double eps = 0.00001; //si un numero es menor que esto, lo considero 0
+        for(int i = 0; i<nodes; i++){
+            for(int j = 0; j<nodes; j++){
+                if(goles[i]<eps){
+                    h[i][j] = 1.0/nodes;
+                } else {
+                    // ver paper para esta cuenta
+                    h[i][j] = c * h[i][j] / goles[i]  +  (1-c) / nodes;
+                }
+            } 
+        }
+        // m es h transpuesta
+        for(int i = 0; i<nodes; i++){
+            for(int j = 0; j<nodes; j++){
+                m[i][j] = h[j][i];
+            } 
+        }
+
+
+        for(int i = 0; i<nodes; i++){
+            mostrar_vector(m[i], std::cerr);
+            std::cerr << std::endl;
+        }
+
+        std::cerr << "listo" << std::endl;
+    }
+
+    uint get_nodes(){
+        return m.size();
+    }
+
+    std::vector<double> multiplicar(std::vector<double> x){
+        std::vector<double> y;
+
+        for(uint i = 0; i<m.size(); i++){
+            double accum = 0;
+            for(uint j = 0; j<m[i].size(); j++){
+                accum += x[j] *((double) m[i][j]);
+            }
+            y.push_back(accum);
+        }
+        return y;
+    }
+
+
+
     
 
 private:
-    std::vector<std::vector<uint> >  m;
+    std::vector<std::vector<double> >  m;
 };
 
 #endif // MATRIZ_DEPORTES_H
--- src/problema.h
+++ src/problema.h
@@ -1,4 +1,7 @@
 #include "matriz.h"
+#include "matriz_deportes.h"
+#include <unistd.h>
+
 
 
 // la distancia basada en norma 1
@@ -17,6 +20,20 @@ double norma_1(std::vector<double> v){
 }
 
 
+double dist_2(std::vector<double> v, std::vector<double> w){
+    double dist = 0;
+    for(uint i = 0; i<v.size(); i++) 
+        dist += pow(v[i] - w[i], 2);
+    return sqrt(dist);
+}
+
+double norma_2(std::vector<double> v){
+    double suma = 0;
+    for(uint i = 0; i<v.size(); i++)
+        suma += v[i]*v[i];
+    return sqrt(suma);
+}
+
 std::vector<double> pagerank(Matriz p_trans, double c, double tolerancia){
     // uso el algoritmo 1 de kamvar et al.
     uint nodes = p_trans.get_nodes();
@@ -26,20 +43,22 @@ std::vector<double> pagerank(Matriz p_trans, double c, double tolerancia){
     uint k = 0;
      
     while(1){
-        //std::cerr << "iteracion "<< k++ << std::endl;
-        
+        //std::cerr << "iteracion "<< k++ << std::endl;        
+        mostrar_vector(x, std::cout);
+        std::cerr << std::endl;
+        usleep(100000);
         y = p_trans.multiplicar(x);
         for(uint i = 0; i<y.size(); i++)
             y[i] *= c;
     
         w = norma_1(x) - norma_1(y);
-   
-    
+ 
         for(uint i = 0; i<y.size(); i++)
             y[i] += w*v[i];
 
         // si la distancia entre Ax y x es menor a tolerancia, terminamos
         // si no, seguimos
+        std::cerr << dist_1(x,y) <<std::endl;
         if(dist_1(x,y) < tolerancia)
             return y;
         else
@@ -52,6 +71,27 @@ std::vector<uint> indeg(Matriz p_trans){
     return p_trans.get_in_degrees();
 }
 
+std::vector<double> metodopot(MatrizDep p, double tolerancia){
+    uint nodes = p.get_nodes();
+    std::vector<double> x(nodes, 1./nodes), y;
+    double n2;
+    while(1){
+        //std::cerr << "iteracion "<< k++ << std::endl;        
+        y = p.multiplicar(x);
+
+        n2 = norma_1(y);
+        for(int i = 0; i<y.size(); i++)
+            y[i] /= n2;
+
+        // si la distancia entre Ax y x es menor a tolerancia, terminamos
+        // si no, seguimos 
+        if(dist_1(x,y) < tolerancia)
+            return y;
+        else
+            x = y;
+    }
+    return y;
+}
 
 template <typename T>
 void solucion(std::vector<T> v, std::ofstream& solus_file){
@@ -84,6 +124,13 @@ void resolver(int tipo_de_instancia,
             solucion(pagerank(p_trans, c, tolerancia), solus_file);
         else // in-deg
             solucion(indeg(p_trans), solus_file);
+    } else { // LIGAS DEPORTIVAS
+        MatrizDep h_trans(red_file, c);
+        red_file.close();
+        if(alg == 0) // GeM
+            solucion(metodopot(h_trans, tolerancia), solus_file);
+        else // to be decided
+            ;
     }
 
     solus_file.close();
